78fe6e5b06b9cf39357109c88b27a48d9b8543d2
BadtzMaru <707380414@qq.com>
2021-01-17 22:58:22 +0800

    集成redux

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 import AppNavigators from './js/navigator/AppNavigators';
+import App from './js/App';
 
 console.disableYellowBox = true;
 
-AppRegistry.registerComponent(appName, () => AppNavigators);
+AppRegistry.registerComponent(appName, () => App);
---
+++ js/reducer/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import theme from './theme';
+
+// 合并 reducer
+const index = combineReducers({theme});
+
+export default index;
